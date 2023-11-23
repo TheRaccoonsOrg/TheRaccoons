0f1c8e4c81501c63d4a42ebc42499c09bd77b9cb
Mihail Danilov <mihaildanilov793@gmail.com>
2023-11-23 19:05:45 +0200

    chore: address UI feedback for home page

--- frontend/components/layout/friends.tsx
+++ frontend/components/layout/friends.tsx
@@ -7,7 +7,7 @@ const FriendsSection = () => {
   const tImage = useTranslations('Image');
 
   return (
-    <div className="flex flex-col justify-center items-center my-10 ">
+    <div className="flex flex-col max-w-[900px] justify-center items-center my-10 mx-auto">
       <div className="w-[200px] mb-2">
         <Image src={tImage('friends')} width={500} height={500} alt="Friends / Draugi" />
       </div>
@@ -15,7 +15,7 @@ const FriendsSection = () => {
         {friendsImagePaths.map((item, index) =>
           item.show ? (
             <div
-              className="w-1/2 sm:w-1/4 md:w-1/4 py-2 flex items-center justify-center"
+              className="w-1/2 sm:w-1/4 md:w-1/4 p-3 flex items-center justify-center"
               key={index}>
               <Link href={item.link} rel="noopener noreferrer" target="_blank">
                 <Image
