fb200799cf8c21a55279dccc83825df13241366b
Mihail Danilov <mihaildanilov793@gmail.com>
2024-01-25 20:57:19 +0200

    fix(styles): add responsive UI for test description

--- frontend/app/[locale]/(routes)/stories/_components/testDescription.tsx
+++ frontend/app/[locale]/(routes)/stories/_components/testDescription.tsx
@@ -4,8 +4,8 @@ import Image from 'next/image';
 const TestDescription = () => {
   const t = useTranslations('Stories.testSection');
   return (
-    <div className="mt-10 flex flex-row mx-10 gap-x-6">
-      <div className=" flex flex-col max-w-[450px] gap-y-4">
+    <div className="mt-10 flex flex-col lg:flex-row mx-10 gap-x-6 gap-y-6 items-center">
+      <div className=" flex flex-col max-w-[700px] lg:max-w-[450px] gap-y-4">
         <p>{t('description.text.first')}</p>
         <p>{t('description.text.second')}</p>
         <p>{t('description.text.third')}</p>
