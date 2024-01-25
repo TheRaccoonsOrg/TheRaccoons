162cc077e3725c05b5b68255821a30b1be6074e2
Mihail Danilov <mihaildanilov793@gmail.com>
2024-01-25 21:26:55 +0200

    fix(styles): change max width property of header and column section

--- frontend/app/[locale]/(routes)/stories/_components/header.tsx
+++ frontend/app/[locale]/(routes)/stories/_components/header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
 
   return (
     <div className="flex flex-col-reverse items-center lg:flex-row gap-y-4 md:gap-x-10 mx-10">
-      <div className="flex flex-col lg:max-w-[500px] gap-y-4 ">
+      <div className="flex flex-col max-w-[700px] lg:max-w-[500px] gap-y-4 ">
         <p>{t('first')}</p>
         <p>{t('second')}</p>
         <p>{t('third')}</p>
