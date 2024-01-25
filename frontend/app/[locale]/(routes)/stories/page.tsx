f12c41cf461975eed640459833e824fccc6a1769
Mihail Danilov <mihaildanilov793@gmail.com>
2024-01-25 19:48:26 +0200

    feat(stories): add video player

--- frontend/app/[locale]/(routes)/stories/page.tsx
+++ frontend/app/[locale]/(routes)/stories/page.tsx
@@ -1,11 +1,13 @@
 import ColumnSection from './_components/columnSection';
 import Header from './_components/header';
+import VideoPlayer from './_components/videoPlayer';
 
 const Stories = () => {
   return (
     <div className="flex flex-col items-center pt-4">
       <Header />
       <ColumnSection />
+      <VideoPlayer />
     </div>
   );
 };
