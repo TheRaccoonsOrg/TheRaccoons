9955d9d47beda309ac5d2a9c688959ff9c2d5def
Mihail Danilov <mihaildanilov793@gmail.com>
2024-01-25 21:12:01 +0200

    fix(styles): reverse flex in test description section

--- frontend/app/[locale]/(routes)/stories/_components/testDescription.tsx
+++ frontend/app/[locale]/(routes)/stories/_components/testDescription.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 const TestDescription = () => {
   const t = useTranslations('Stories.testSection');
   return (
-    <div className="mt-10 flex flex-col lg:flex-row mx-10 gap-x-6 gap-y-6 items-center">
+    <div className="mt-10 flex flex-col-reverse lg:flex-row mx-10 gap-x-6 gap-y-6 items-center">
       <div className=" flex flex-col max-w-[700px] lg:max-w-[450px] gap-y-4">
         <p>{t('description.text.first')}</p>
         <p>{t('description.text.second')}</p>
