c32220469931b50c5af66cd92f651042a88bd311
Mihail Danilov <mihaildanilov793@gmail.com>
2024-02-08 09:36:54 +0200

    feat(workshop): responsive design implemented

--- frontend/app/[locale]/(routes)/events/(workshops)/[slug]/page.tsx
+++ frontend/app/[locale]/(routes)/events/(workshops)/[slug]/page.tsx
@@ -17,17 +17,17 @@ const Workshop2023 = ({ params }: { params: { slug: string } }) => {
   }
 
   return (
-    <div className="max-w-[924px] mx-auto">
-      <h1 className="text-6xl font-raccoons text-hotgreen text-center pt-10">
+    <div className="mx-10 lg:mx-auto max-w-[900px]">
+      <h1 className="text-5xl md:text-6xl font-raccoons text-hotgreen text-center pt-10">
         {filteredWorkshops.title}
       </h1>
       <p className="text-sm font-bold text-hotgreen pt-10">{filteredWorkshops.date}</p>
       <p className="text-sm font-bold text-hotgreen ">{filteredWorkshops.place}</p>
       <p className="text-sm">{filteredWorkshops.eventShortDescription}</p>
       <div className="flex flex-col items-center mt-10">
-        <div className="bg-hotgreen rounded-lg w-[629px] h-[349px] bg-opacity-40 flex items-center justify-center border-hotgreen border-2">
+        <div className="bg-hotgreen rounded-lg w-[390px] h-[210px] md:w-[629px] md:h-[349px] bg-opacity-40 flex items-center justify-center border-hotgreen border-2">
           <Image
-            className="flex items-center justify-center w-[594px] h-[312px]"
+            className="flex items-center justify-center w-[360px] h-[190]px md:w-[594px] md:h-[312px]"
             src={filteredWorkshops.image.src}
             alt={filteredWorkshops.image.alt}
             width={filteredWorkshops.image.width}
@@ -44,34 +44,42 @@ const Workshop2023 = ({ params }: { params: { slug: string } }) => {
       <div className="flex flex-row mt-1 ">
         <p className="text-sm mr-1">{filteredWorkshops.moreInfo} </p>
         {filteredWorkshops.moreInfoLink ? (
-          <Link className="text-sm underline" href={filteredWorkshops.moreInfoLink}>
+          <Link
+            className="text-sm underline"
+            href={filteredWorkshops.moreInfoLink}
+            rel="noopener noreferrer"
+            target="_blank">
             {filteredWorkshops.moreInfoLink}
           </Link>
         ) : null}
       </div>
       <p className="text-sm mt-5 font-bold">{filteredWorkshops.description2nd}</p>
-      <div className="flex flex-row mt-5 ">
+      <div className="flex flex-col md:flex-row mt-5 ">
         <p className="text-sm font-bold text-hotgreen mr-1">Kad?</p>
         <p className="text-sm">{filteredWorkshops.when}</p>
       </div>
-      <div className="flex flex-row">
+      <div className="flex flex-col md:flex-row mt-2">
         <p className="text-sm font-bold text-hotgreen mr-1">Kur?</p>
         <p className="text-sm">{filteredWorkshops.where}</p>
       </div>
-      <div className="flex flex-row mt-5 ">
+      <div className="flex flex-col md:flex-row mt-2 ">
         <p className="text-sm font-bold text-hotgreen mr-1">Kas jāņem līdzi?</p>
         <p className="text-sm">{filteredWorkshops.necessary}</p>
       </div>
-      <div className="flex flex-row mt-5 ">
+      <div className="flex flex-col md:flex-row mt-2 ">
         <p className="text-sm font-bold text-hotgreen mr-1">Reģistrējies darbnīcai te:</p>
-        <Link className="text-sm underline text-purple-br" href={filteredWorkshops.registerLink}>
+        <Link
+          className="text-sm underline text-purple-br"
+          href={filteredWorkshops.registerLink}
+          rel="noopener noreferrer"
+          target="_blank">
           {filteredWorkshops.registerLink}
         </Link>
       </div>
-      <div className="mt-10">
+      <div className="mt-4">
         <p className="text-sm font-bold text-hotgreen">Ko vari sagaidīt no darbnīcas?</p>
         {filteredWorkshops.timeFrames.map((timeFrame, index) => (
-          <div key={index} className="flex flex-col mt-5">
+          <div key={index} className="flex flex-col mt-1">
             <p className="text-sm  text-hotgreen mr-1">{timeFrame.timeFrame}</p>
             <ul>
               {timeFrame.descriptionOfTimeFrame.map((item, index) => (
