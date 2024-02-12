1fa66869c4708eca049a30fd0a5aac0975d0e59b
Mihail Danilov <mihaildanilov793@gmail.com>
2024-02-12 19:44:01 +0200

    feat(main): add image loading effect

--- frontend/components/InfoCard.tsx
+++ frontend/components/InfoCard.tsx
@@ -1,12 +1,28 @@
+'use client';
 import { InfoCardProps } from '@/types';
 import Image from 'next/image';
 import Link from 'next/link';
 import GreenButton from './green-button';
+import { useState } from 'react';
+import { SkeletonLoader } from './hackathon/skeletons/skeleton-loader';
 
 const InfoCard = (props: InfoCardProps) => {
+  const [loading, setLoading] = useState(true);
   return (
     <div className="w-[350px] md:w-[300px] flex flex-col justify-center items-start mb-3">
-      <Image src={props.imagePath} alt={props.alt} width={props.width} height={props.height} />
+      <div className="w-full h-[136px]">
+        {loading && <SkeletonLoader green={false} />}
+        <Image
+          src={props.imagePath}
+          alt={props.alt}
+          width={props.width}
+          height={props.height}
+          onLoadingComplete={() => setLoading(false)}
+          className={` transition-opacity duration-500 rounded-md ${
+            loading ? 'opacity-0' : 'opacity-100'
+          }`}
+        />
+      </div>
       <p className="text-lg my-5">{props.text}</p>
       <div className="mx-auto md:mx-0 flex flex-col justify-center items-center md:items-start">
         {props.buttonText && props.buttonHref ? (
--- frontend/components/layout/hero.tsx
+++ frontend/components/layout/hero.tsx
@@ -1,4 +1,7 @@
+'use client';
 import Image from 'next/image';
+import { useState } from 'react';
+import { SkeletonLoader } from '../hackathon/skeletons/skeleton-loader';
 
 interface HeroHeaderProps {
   firstDesc: string;
@@ -7,6 +10,8 @@ interface HeroHeaderProps {
   imageAlt: string;
 }
 const HeroHeader = (props: HeroHeaderProps) => {
+  const [loading, setLoading] = useState(true);
+
   return (
     <header className={`flex flex-col justify-center items-center md:flex-row gap-x-4 `}>
       <div className="w-[350px] sm:w-[400px] md:w-[450px]">
@@ -14,8 +19,20 @@ const HeroHeader = (props: HeroHeaderProps) => {
         <br />
         <p>{props.secondDesc}</p>
       </div>
-      <div className="w-[350px] lg:w-[450px]">
-        <Image priority src={props.imageSrc} alt={props.imageAlt} width={800} height={800} />
+      <div className="w-[350px] h-[350px] lg:w-[450px] lg:h-[450px]">
+        {loading && <SkeletonLoader green={false} />}
+
+        <Image
+          priority
+          src={props.imageSrc}
+          alt={props.imageAlt}
+          width={800}
+          height={800}
+          onLoadingComplete={() => setLoading(false)}
+          className={` transition-opacity duration-500 rounded-md ${
+            loading ? 'opacity-0' : 'opacity-100'
+          }`}
+        />
       </div>
     </header>
   );
