ffb13f5f0574f87567b3946c62705f883bb026fe
Mihail Danilov <mihaildanilov793@gmail.com>
2024-02-12 19:44:01 +0200

    feat(main): add image loading effect

--- frontend/components/layout/about-us.tsx
+++ frontend/components/layout/about-us.tsx
@@ -1,11 +1,26 @@
+'use client';
 import { InfoCardProps } from '@/types';
 import Image from 'next/image';
+import { useState } from 'react';
+import { SkeletonLoader } from '../hackathon/skeletons/skeleton-loader';
 
 const AboutUs = (props: InfoCardProps) => {
+  const [loading, setLoading] = useState(true);
+
   return (
     <div className="mt-8 flex flex-col items-center justify-center gap-x-10 md:flex-row">
-      <div className="w-[350px] md:w-[400px]">
-        <Image src={props.imagePath} alt="Image" width={1000} height={1000} />
+      <div className="w-[350px] h-[161px] md:w-[400px] md:h-[184px]">
+        {loading && <SkeletonLoader green={false} />}
+        <Image
+          src={props.imagePath}
+          alt="Image"
+          width={1000}
+          height={1000}
+          onLoadingComplete={() => setLoading(false)}
+          className={` transition-opacity duration-500 rounded-md ${
+            loading ? 'opacity-0' : 'opacity-100'
+          }`}
+        />
       </div>
       <p className="mt-4 w-[350px] md:w-[500px] ">{props.text}</p>
     </div>
