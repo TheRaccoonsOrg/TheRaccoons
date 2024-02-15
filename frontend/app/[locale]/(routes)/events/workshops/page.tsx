30e2486e23d5cc76e1b217c05e1e537790f63374
Mihail Danilov <mihaildanilov793@gmail.com>
2024-02-15 21:35:36 +0200

    chore(events): add contact section

--- frontend/app/[locale]/(routes)/events/workshops/page.tsx
+++ frontend/app/[locale]/(routes)/events/workshops/page.tsx
@@ -3,6 +3,7 @@ import { EventTypes } from '@/types';
 import { useTranslations } from 'next-intl';
 import EventCardGrid from '../_components/event-card-grid';
 import EventPageNav from '../_components/event-page-nav';
+import ContactSection from '@/components/layout/contact-info';
 
 const EventsPage = () => {
   const filteredEvents = GetEventList('workshop' as EventTypes);
@@ -22,12 +23,15 @@ const EventsPage = () => {
     },
   ];
   return (
-    <div>
-      <div className="flex flex-col justify-center md:justify-start mx-5 md:mx-20">
+    <div className="flex flex-col justify-between min-h-screen ">
+      <div className=" mx-5 md:mx-20 mt-4">
         <EventPageNav events={eventTypes} placeHolder={t('sort')} />
       </div>
       <div className="flex flex-wrap items-center justify-center">
         <EventCardGrid props={filteredEvents} />
+      </div>
+      <div className="mt-auto mb-20">
+        <ContactSection />
       </div>
     </div>
   );
