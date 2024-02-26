600b519a068eb2aa0fb2a5d386a56c97d8503db4
Mihail Danilov <mihaildanilov793@gmail.com>
2024-02-26 19:00:47 +0200

    feat: handle smaller screen sizes

--- frontend/app/[locale]/(routes)/events/_components/event-card.tsx
+++ frontend/app/[locale]/(routes)/events/_components/event-card.tsx
@@ -4,8 +4,8 @@ import { EventCardProps } from '@/types';
 
 const EventCard = (props: EventCardProps) => {
   return props.show ? (
-    <div className="m-3 flex min-h-[300px] w-[400px] h-[352px] flex-col items-center justify-around rounded-lg border-2 border-purple-br bg-purple-md py-3">
-      <h2 className="font-raccoons text-4xl">{props.title}</h2>
+    <div className="m-3 flex min-h-[300px] w-[340px] sm:w-[350px] h-[352px] flex-col items-center justify-around rounded-lg border-2 border-purple-br bg-purple-md py-3">
+      <h2 className="font-raccoons text-3xl md:text-4xl">{props.title}</h2>
       {props.cardImage ? (
         <div className="w-[300px] h-[200px]">
           <ImageWithSkeleton
