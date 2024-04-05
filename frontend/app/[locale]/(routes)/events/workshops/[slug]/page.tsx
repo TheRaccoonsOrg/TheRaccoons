d81d80bcd7d348deced7e99de1bb1ac703f091cb
Mihail Danilov <mihaildanilov793@gmail.com>
2024-04-06 00:08:48 +0300

    feat: refactor workshops data

--- frontend/app/[locale]/(routes)/events/workshops/[slug]/page.tsx
+++ frontend/app/[locale]/(routes)/events/workshops/[slug]/page.tsx
@@ -1,94 +1,91 @@
 'use client';
 import { useRouter } from 'next/navigation';
-import { workshopsData } from '../_data/workshopsData';
 import GreenButton from '@/components/green-button';
 import Link from 'next/link';
 import ContactSection from '@/components/layout/contact-info';
 import ImageWithSkeleton from '@/components/hackathon/skeletons/image-with-skeleton';
+import { findWorkshopBySlug } from '@/actions/events';
 
-const Workshop2023 = ({ params }: { params: { slug: string } }) => {
+const Workshop = ({ params }: { params: { slug: string } }) => {
   const router = useRouter();
-  function findWorkshopBySlug(slug: string) {
-    return workshopsData.find((workshop) => workshop.slug === slug);
-  }
-  const filteredWorkshops = findWorkshopBySlug(params.slug);
-  if (!filteredWorkshops) {
+  const workshop = findWorkshopBySlug(params.slug);
+  if (!workshop) {
     return router.push('/404');
   }
 
   return (
     <div className="flex flex-col mx-10 lg:mx-auto max-w-[900px] h-[91vh]">
       <h1 className="text-5xl md:text-6xl font-raccoons text-hotgreen text-center pt-10">
-        {filteredWorkshops.title}
+        {workshop.title}
       </h1>
-      <p className="text-sm font-bold text-hotgreen pt-10">{filteredWorkshops.date}</p>
-      <p className="text-sm font-bold text-hotgreen ">{filteredWorkshops.place}</p>
-      <p className="text-sm">{filteredWorkshops.eventShortDescription}</p>
+      <p className="text-sm font-bold text-hotgreen pt-10">{workshop.dateWithText}</p>
+      <p className="text-sm font-bold text-hotgreen ">{workshop.place}</p>
+      <p className="text-sm">{workshop.eventShortDescription}</p>
       <div className="flex flex-col items-center mt-10">
         <div className="bg-hotgreen rounded-lg w-[390px] h-[220px] md:w-[629px] md:h-[349px] bg-opacity-40 flex p-4 items-center justify-center border-hotgreen border-2">
           <ImageWithSkeleton
             priority={true}
             green={true}
-            src={filteredWorkshops.image.src}
-            alt={filteredWorkshops.image.alt}
-            width={filteredWorkshops.image.width}
-            height={filteredWorkshops.image.height}
+            src={workshop.image.src}
+            alt={workshop.image.alt}
+            width={workshop.image.width}
+            height={workshop.image.height}
             imageStyles="object-fit w-[354px] h-[186px] md:w-[593px] md:h-[312px]"
           />
         </div>
-        {filteredWorkshops.registerLink ? (
+        {workshop.registerLink ? (
           <GreenButton
-            buttonHref={filteredWorkshops.registerLink}
+            buttonHref={workshop.registerLink}
             buttonText="Piesakies"
             buttonStyles="w-[150px] mt-5"
           />
         ) : null}
       </div>
-      <p className="text-sm mt-10">{filteredWorkshops.description}</p>
+      <p className="text-sm mt-10">{workshop.description}</p>
       <div className="flex flex-row mt-1 ">
-        <p className="text-sm mr-1">{filteredWorkshops.moreInfo} </p>
-        {filteredWorkshops.moreInfoLink ? (
+        <p className="text-sm mr-1">{workshop.moreInfo} </p>
+        {workshop.moreInfoLink ? (
           <Link
             className="text-sm underline"
-            href={filteredWorkshops.moreInfoLink}
+            href={workshop.moreInfoLink}
             rel="noopener noreferrer"
             target="_blank">
-            {filteredWorkshops.moreInfoLink}
+            {workshop.moreInfoLink}
           </Link>
         ) : null}
       </div>
-      {filteredWorkshops.description2nd ? (
+      {workshop.description2nd ? (
         <>
-          <p className="text-sm mt-5 font-bold">{filteredWorkshops.description2nd}</p>
+          <p className="text-sm mt-5 font-bold">{workshop.description2nd}</p>
           <div className="flex flex-col md:flex-row mt-5 ">
             <p className="text-sm font-bold text-hotgreen mr-1">Kad?</p>
-            <p className="text-sm">{filteredWorkshops.when}</p>
+            <p className="text-sm">{workshop.when}</p>
           </div>
         </>
       ) : null}
-      {filteredWorkshops.description2nd ? (
+      {workshop.description2nd ? (
         <>
           <div className="flex flex-col md:flex-row mt-2">
             <p className="text-sm font-bold text-hotgreen mr-1">Kur?</p>
-            <p className="text-sm">{filteredWorkshops.where}</p>
+            <p className="text-sm">{workshop.where}</p>
           </div>
           <div className="flex flex-col md:flex-row mt-2 ">
             <p className="text-sm font-bold text-hotgreen mr-1">Kas jāņem līdzi?</p>
-            <p className="text-sm">{filteredWorkshops.necessary}</p>
+            <p className="text-sm">{workshop.necessary}</p>
           </div>
           <div className="flex flex-col md:flex-row mt-2 ">
             <p className="text-sm font-bold text-hotgreen mr-1">Reģistrējies darbnīcai te:</p>
             <Link
               className="text-sm underline text-purple-br"
-              href={filteredWorkshops.registerLink}
+              href={workshop.registerLink}
               rel="noopener noreferrer"
               target="_blank">
-              {filteredWorkshops.registerLink}
+              {workshop.registerLink}
             </Link>
           </div>
           <div className="mt-4">
             <p className="text-sm font-bold text-hotgreen">Ko vari sagaidīt no darbnīcas?</p>
-            {(filteredWorkshops.timeFrames ?? []).map((timeFrame, index) => (
+            {(workshop.timeFrames ?? []).map((timeFrame, index) => (
               <div key={index} className="flex flex-col mt-1">
                 <p className="text-sm  text-hotgreen mr-1">{timeFrame.timeFrame}</p>
                 <ul>
@@ -102,7 +99,7 @@ const Workshop2023 = ({ params }: { params: { slug: string } }) => {
             ))}
           </div>
           <div className="mt-10">
-            {filteredWorkshops.subInfoSection.map((item, index) => (
+            {workshop.subInfoSection.map((item, index) => (
               <p className="text-sm" key={index}>
                 {item.shortDescription}
               </p>
@@ -118,4 +115,4 @@ const Workshop2023 = ({ params }: { params: { slug: string } }) => {
   );
 };
 
-export default Workshop2023;
+export default Workshop;
