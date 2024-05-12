2f691861b93883b25fec5cdff263be2addc80ec2
Mihail Danilov <mihaildanilov793@gmail.com>
2024-05-12 23:24:30 +0300

    chore(testing): configure Jest and add initial test for HeaderSection component

---
+++ frontend/__tests__/test-util.tsx
@@ -0,0 +1,24 @@
+// test-utils.js
+
+import React from 'react';
+import { render as rtlRender, RenderOptions } from '@testing-library/react';
+import { NextIntlClientProvider } from 'next-intl';
+
+import { ReactElement } from 'react';
+
+type CustomRenderOptions = {
+  locale?: string;
+} & RenderOptions;
+
+const render = (ui: ReactElement, options?: CustomRenderOptions) => {
+  const { locale = 'en', ...rtlOptions } = options || {};
+
+  function Wrapper({ children }: { children?: React.ReactNode }) {
+    return <NextIntlClientProvider locale={locale}>{children}</NextIntlClientProvider>;
+  }
+
+  return rtlRender(ui, { wrapper: Wrapper, ...rtlOptions });
+};
+
+export * from '@testing-library/react';
+export { render };
