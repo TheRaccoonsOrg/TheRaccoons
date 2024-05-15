784425504d5e280f6e9dba3ec2f2c3ef9f3b7b8e
Mihail Danilov <mihaildanilov793@gmail.com>
2024-05-15 16:43:59 +0300

    feat: add unit test for ContactSection component

---
+++ frontend/__tests__/components/layout/ContactSections.test.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import ContactSection from '@/components/layout/ContactSection';
+import { contactConfig } from '@/config/site';
+describe('ContactSection', () => {
+  it('renders links to social media platforms', async () => {
+    render(<ContactSection />);
+
+    expect(screen.getAllByRole('link')[0]).toHaveAttribute(
+      'href',
+      'https://www.facebook.com/RaccoonsHQ',
+    );
+    expect(screen.getAllByRole('link')[1]).toHaveAttribute(
+      'href',
+      'https://www.instagram.com/raccoonshq/',
+    );
+    expect(screen.getAllByRole('link')[2]).toHaveAttribute(
+      'href',
+      'https://www.linkedin.com/company/the-raccoons-student-community/',
+    );
+    expect(screen.getAllByRole('link')[3]).toHaveAttribute(
+      'href',
+      'https://twitter.com/RaccoonsHQ',
+    );
+    expect(screen.getAllByRole('link')[4]).toHaveAttribute(
+      'href',
+      'https://github.com/TheRaccoonsOrg',
+    );
+  });
+
+  it('renders email link', async () => {
+    render(<ContactSection />);
+    expect(screen.getByText(contactConfig.email)).toBeInTheDocument();
+    expect(screen.getAllByRole('link')[5]).toHaveAttribute('href', `mailto:${contactConfig.email}`);
+  });
+
+  test('renders social media icons', () => {
+    render(<ContactSection />);
+    expect(screen.getByLabelText('Facebook')).toBeInTheDocument();
+    expect(screen.getByLabelText('Instagram')).toBeInTheDocument();
+    expect(screen.getByLabelText('LinkedIn')).toBeInTheDocument();
+    expect(screen.getByLabelText('Twitter')).toBeInTheDocument();
+    expect(screen.getByLabelText('GitHub')).toBeInTheDocument();
+  });
+});
--- frontend/components/layout/ContactSection.tsx
+++ frontend/components/layout/ContactSection.tsx
@@ -6,19 +6,21 @@ const ContactSection = () => {
   return (
     <div className="my-4 flex flex-col items-center justify-center gap-y-4">
       <div className="flex flex-row gap-x-2">
-        <Link href="https://www.facebook.com/RaccoonsHQ">
+        <Link href="https://www.facebook.com/RaccoonsHQ" aria-label="Facebook">
           <FaFacebook size={40} />
         </Link>
-        <Link href="https://www.instagram.com/raccoonshq/">
+        <Link href="https://www.instagram.com/raccoonshq/" aria-label="Instagram">
           <FaInstagram size={40} />
         </Link>
-        <Link href="https://www.linkedin.com/company/the-raccoons-student-community/">
+        <Link
+          href="https://www.linkedin.com/company/the-raccoons-student-community/"
+          aria-label="LinkedIn">
           <FaLinkedin size={40} />
         </Link>
-        <Link href="https://twitter.com/RaccoonsHQ">
+        <Link href="https://twitter.com/RaccoonsHQ" aria-label="Twitter">
           <FaTwitter size={40} />
         </Link>
-        <Link href="https://github.com/TheRaccoonsOrg">
+        <Link href="https://github.com/TheRaccoonsOrg" aria-label="GitHub">
           <FaGithub size={40} />
         </Link>
       </div>
