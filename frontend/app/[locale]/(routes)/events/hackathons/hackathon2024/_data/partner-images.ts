1aa0580bea4de26b3145d05e5e113270d0842628
Mihail Danilov <mihaildanilov793@gmail.com>
2024-07-25 10:06:38 +0300

    ftr: add new hackathon page

---
+++ frontend/app/[locale]/(routes)/events/hackathons/hackathon2024/_data/partner-images.ts
@@ -0,0 +1,99 @@
+import { PartnerImagesProps } from '@/types';
+
+export const partnerImages: PartnerImagesProps[] = [
+  {
+    imagePath: '/images/partners2023/liaa_logo.webp',
+    alt: 'LIAA Logo',
+    width: 600,
+    height: 94,
+    linkHref: 'https://www.liaa.gov.lv/en/liaa-technology-business-center',
+  },
+  {
+    imagePath: '/images/partners2023/CFI Logo White.webp',
+    alt: 'CFI Logo White',
+    width: 471,
+    height: 82,
+    linkHref: 'https://www.cfi.lu.lv/',
+  },
+  {
+    imagePath: '/images/partners2023/CFI Fonds White.webp',
+    alt: 'CFI Fonds White',
+    width: 164,
+    height: 82,
+    linkHref: 'https://www.cfi.lu.lv/',
+  },
+  {
+    imagePath: '/images/partners2023/logo_5_lat_0.webp',
+    alt: 'Logo 5 Lat',
+    width: 133,
+    height: 103,
+    linkHref: 'https://www.em.gov.lv/',
+  },
+
+  {
+    imagePath: '/images/partners2023/Riga—LV—Logo-white 1.webp',
+    alt: 'Riga LV Logo White',
+    width: 202,
+    height: 93,
+    linkHref: 'https://www.riga.lv/lv',
+  },
+  {
+    imagePath: '/images/partners2023/Estoty_logo.webp',
+    alt: 'Estoty Logo',
+    width: 220,
+    height: 93,
+    linkHref: 'https://estoty.com/',
+  },
+  {
+    imagePath: '/images/partners2023/echo3D - Logo 2021 (1).webp',
+    alt: 'echo3D Logo 2021',
+    width: 218,
+    height: 35,
+    linkHref: 'https://www.echo3d.com/',
+  },
+  {
+    imagePath: '/images/partners2023/Ir logo 600x600px (1).webp',
+    alt: 'Ir Logo 600x600px',
+    width: 75,
+    height: 75,
+    linkHref: 'https://ir.lv/',
+  },
+];
+
+export const communityPartners: PartnerImagesProps[] = [
+  {
+    imagePath: '/images/friends/visas-iespejas.webp',
+    alt: 'Visas iespejas logo',
+    width: 176,
+    height: 76,
+    linkHref: 'https://visasiespejas.lv/',
+  },
+  {
+    alt: 'Riga Tech Girls',
+    imagePath: '/images/friends/riga-tech-girls.webp',
+    width: 188,
+    height: 76,
+    linkHref: 'http://rigatechgirls.org',
+  },
+  {
+    alt: 'GameDev.lv',
+    imagePath: '/images/friends/gamedev-lv.webp',
+    width: 82,
+    height: 76,
+    linkHref: 'https://www.gamedev.lv',
+  },
+  {
+    alt: 'DF SP Logo Balts',
+    imagePath: '/images/friends/DF_SP_logo_balts.webp',
+    width: 94,
+    height: 76,
+    linkHref: 'https://datoriki.lv',
+  },
+  {
+    alt: 'TechChill',
+    imagePath: '/images/friends/techchill.webp',
+    width: 236,
+    height: 76,
+    linkHref: 'https://www.techchill.co',
+  },
+];
---
+++ frontend/app/[locale]/(routes)/events/hackathons/hackathon2024/page.tsx
@@ -0,0 +1,65 @@
+import GreenButton from '@/components/GreenButton';
+import HowDidItGo from './_components/HowDidItGo';
+import Winners from './_components/Winners';
+import Categories from '@/components/hackathon/Categories';
+import ContactSection from '@/components/layout/ContactSection';
+import HeaderSection from '@/components/hackathon/header/HeaderSection';
+import { CombinedImageProps } from '@/types';
+import { challengeCategories } from './_data/categories';
+import { communityPartners, partnerImages } from './_data/partner-images';
+import Partners from '@/components/hackathon/Partners';
+import SeeYouNextYear from '@/components/hackathon/SeeYouNextYear';
+
+const headerImage: CombinedImageProps[] = [
+  {
+    imageStyles: 'hidden  md:block md:w-[700px] lg:w-[971px]',
+    src: '/images/raccoons_apply.webp',
+    alt: 'Hackathon2023',
+    imageWidth: 971,
+    imageHeight: 492,
+    isPriority: true,
+    date: '20.10 - 22.10.2023.',
+    place: 'Riga, Latvia',
+  },
+  {
+    imageStyles: 'block w-full md:hidden',
+    src: '/images/raccoons_apply-sm.webp',
+    alt: 'Hackathon2023',
+    imageWidth: 560,
+    imageHeight: 436,
+    isPriority: true,
+    date: '20.10 - 22.10.2023.',
+    place: 'Riga, Latvia',
+  },
+];
+const Hackathon2022 = () => {
+  return (
+    <div className="mx-10 flex flex-col items-center">
+      <HeaderSection
+        props={headerImage}
+        submissionsLink="https://raccoons-2023.devpost.com/project-gallery"
+      />
+      <GreenButton
+        buttonStyles="mt-10"
+        buttonText="Watch Awards Ceremony"
+        buttonHref="https://www.youtube.com/live/-RU_4tzY0Bw"
+      />
+      <HowDidItGo />
+      <Winners />
+      <Categories props={challengeCategories} />
+      <GreenButton
+        buttonStyles="mt-10 min-w-[200px]"
+        buttonHref="https://raccoons-2023.devpost.com/project-gallery"
+        buttonText="All submissions"
+      />
+      <SeeYouNextYear
+        imageSrc="/images/hackathon2023/hackathon2023.webp"
+        buttonHref="https://photos.app.goo.gl/4LfM9CetZzthEywJ6"
+      />
+      <Partners props={{ partnerImages, communityPartners }} />
+      <ContactSection />
+    </div>
+  );
+};
+
+export default Hackathon2022;
