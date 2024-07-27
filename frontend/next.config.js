541e236bc7887db36267a589c40b8bb6a222c32f
Mihail Danilov <mihaildanilov793@gmail.com>
2024-07-27 15:04:13 +0300

    Merge main to dev (#75)
    * feat(events): add missing preview images
    
    * feat(seo): add new keywords for seo
    
    * feat: add code camp 6 page
    
    * feat: refactor workshops data
    
    * chore: fix typo in workshop data
    
    * chore: change Sentry API endpoints
    
    * feat: add code camp 7 page
    
    * feat: add github icon in contact section
    
    * feat: add newsletter form
    
    * feat: update next.js to version 14.2.3
    
    * feat: rename files for consistency and clarity
    
    * chore: rename file FinalPresentations
    
    * refactor(file-naming): standardize file names across all components
    
    * refactor(file-naming): standardize file names across all components, part 2
    
    * chore(testing): configure Jest and add initial test for HeaderSection component
    
    * feat(newsletter-form): update localization strings and add sonner on form submit
    
    * feat: remove .env file duplicate
    
    * feat: add responsive design to form
    
    * feat: add corrrect icon to metadata and fix next-config for env handling
    
    * feat: update next to latest version and export baseUrl for SEO

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
 const withNextIntl = require('next-intl/plugin')('./i18n.ts');
-
 const nextConfig = {
   async redirects() {
     return [
